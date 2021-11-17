be738fa774664acc07c3ad2d165800bf7334c18a
Israellongji <92058297+Israellongji@users.noreply.github.com>
2021-11-17 03:36:13 -0800

    Add files via upload

---
+++ MG&C.js
@@ -0,0 +1,9 @@
+
+		function openNav() {
+			document.getElementById("mysidenav").style.width= "250px";
+		}
+		function closeNav() {
+			document.getElementById("mysidenav").style.width= "0";
+		}
+
+	
